311e14f45d1f2b1139a07ae1abd8679f30ffef90
ajaymahadik3897 <126051814+ajaymahadik3897@users.noreply.github.com>
2025-06-13 16:03:51 +0530

    Create StaticAndNonStaticMethod.java

---
+++ src/main/java/StaticAndNonStaticMethod.java
@@ -0,0 +1,15 @@
+public class StaticAndNonStaticMethod {
+    public static void myClass(){  //static method
+        System.out.println("Print static class");
+    }
+    public void secondClass(){
+        System.out.println("Print Non-static class");
+        System.out.println(33 + "A");  //if only single digit is provided then we can right like '3' also
+    }
+    public static void main(String []arg){
+        StaticAndNonStaticMethod obj = new StaticAndNonStaticMethod();  //Create object for run nonstatic method
+        myClass();         //No need to object //Static method
+        obj.secondClass(); //Object.method name //Nonstatic Method //A
+
+    }
+}
