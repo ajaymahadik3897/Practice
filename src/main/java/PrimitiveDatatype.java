33efed1e38881d97c2555cbdd4582eb05e1accfe
ajay.mahadik <ajaymahadi3897@gmail.com>
2025-06-18 09:32:58 +0530

    adding PrimitiveDatatype

---
+++ src/main/java/PrimitiveDatatype.java
@@ -0,0 +1,25 @@
+public class PrimitiveDatatype {
+    public static void main (String [] a){
+        // Primitive Data Types
+        // byte, short, int, long, float, double, char, boolean
+
+        byte b = 10; // 1 byte
+        short s = 20; // 2 bytes
+        int i = 30; // 4 bytes
+        long l = 40L; // 8 bytes
+        float f = 50.5f; // 4 bytes
+        double d = 60.5d; // 8 bytes // 'd' is optional for double
+        char c = 'A'; // 2 bytes
+        boolean bool = true; // 1 bit
+
+        // Printing the values
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Integer value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Character value: " + c);
+        System.out.println("Boolean value: " + bool);
+    }
+}
