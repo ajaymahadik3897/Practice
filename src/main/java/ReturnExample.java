0bbd1822d9757f3ab6041b05b91f6a141aeaf4aa
ajay.mahadik <ajaymahadi3897@gmail.com>
2025-07-02 12:35:11 +0530

    added interface and encapsulations

--- src/main/java/ReturnExample.java
+++ src/main/java/ReturnExample.java
@@ -1,4 +1,4 @@
-public class ReturnExample {  //return keyword exampl
+public class ReturnExample {  // Class definition for ReturnExample
     public static int add(int a, int b) {
         return a + b; // returns the sum to the caller
     }
@@ -11,6 +11,6 @@ public class ReturnExample {  //return keyword exampl
         int result = re.add(5, 3); // Call the add method and store the result
         System.out.println("Sum: " + result); // Output: Sum: 8  or System.out.println("Sum: " + re.add(5, 3)); --without storing the result in a variable
         // The add method returns the sum of a and b, which is then printed to the console
-
+        // Note: If the add method were static, you could call it directly using the class name without creating an instance.
     }
 }
