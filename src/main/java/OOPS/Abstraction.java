df57ece9d891221b76629aa3c1c71d31c6ae71f8
ajay.mahadik <ajaymahadi3897@gmail.com>
2025-07-02 08:41:10 +0530

    Abstarct class

--- src/main/java/OOPS/Abstraction.java
+++ src/main/java/OOPS/Abstraction.java
@@ -1,4 +1,6 @@
 package OOPS;
+// Abstraction // Abstraction is a process of hiding the implementation details and showing only the essential features of the object. // It is a way to reduce complexity and increase efficiency. // In Java, abstraction can be achieved using abstract classes and interfaces.
+
 
 abstract class demo {   // abstract class //incomplete method is present thats why we use abstract class or we can say that abstract class is a class that cannot be instantiated and may contain abstract methods (methods without a body) as well as concrete methods (methods with a body).
     abstract void AB () ; //inccomplete method that why we use abstract keyword, this method must be implemented in the subclass
