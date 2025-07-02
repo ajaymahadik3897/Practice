5a6b4088abb67ccc6ea11bc3b3bf03e5dbb85c2a
ajay.mahadik <ajaymahadi3897@gmail.com>
2025-07-02 08:28:52 +0530

    Abstarct class

---
+++ src/main/java/OOPS/Abstraction.java
@@ -0,0 +1,25 @@
+package OOPS;
+
+abstract class demo {   // abstract class //incomplete method is present thats why we use abstract class or we can say that abstract class is a class that cannot be instantiated and may contain abstract methods (methods without a body) as well as concrete methods (methods with a body).
+    abstract void AB () ; //inccomplete method that why we use abstract keyword, this method must be implemented in the subclass
+
+    void test (){  // non-abstract method in abstract class
+        System.out.println("Run Test method in abstract class");
+    }
+    static void D () {  // static method in abstract class
+        System.out.println("Run static D method in abstract class");
+    }
+}
+
+public class Abstraction extends demo {  //concrete class that extends abstract class demo
+    void AB (){    // complete abstract method in demo class
+        System.out.println("complete abstract method in Abstraction class");
+    }
+    public static void main (String [] arg){
+        Abstraction obj = new Abstraction();
+        obj.AB();
+        obj.test();
+        Abstraction.D(); //demo.D // obj.D //we can run static method of abstract class using object of subclass or class name
+
+    }
+}
